cfbf8502dcb9da238165c00ea8536702be774e18
Dhyey Mavani <dmavani25@amherst.edu>
2023-11-11 11:52:01 -0500

    update OOP implementation of the simulation framework

--- API/main.py
+++ API/main.py
@@ -1,6 +1,8 @@
 #### RUN THIS TO INSTALL PKGS ########
 # python -m pip install semantic-kernel
 ######################################
+
+# Import necessary packages
 import semantic_kernel as sk
 from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
 
@@ -11,40 +13,50 @@ api_key, org_id = sk.openai_settings_from_dot_env()
 kernel.add_chat_service("chat-gpt", OpenAIChatCompletion("gpt-3.5-turbo", api_key, org_id))
 
 # Define prompts
-class LectureAgent:
-    give_lecture = kernel.create_semantic_function("""Give a very concise lecture on {{$INPUT}}""", max_tokens=100)
-    ask_question = kernel.create_semantic_function("""Answer the following question given to your lecture: {{$INPUT}}""")
-class StudentAgent:    
-    generate_questions = kernel.create_semantic_function("""Do you have any questions on the following lecture? Do you not undersand or need more clearity on anything? {{$INPUT}}""")
+class ProfessorAgent:
+    def __init__(self, expertise_area):
+        self.expertise_area = expertise_area
 
-    load_question_answer = kernel.create_semantic_function("""Read the following question and answer carefully, and make sure you understand: {{$INPUT}}""")
+    async def give_lecture(self, topic):
+        return kernel.create_semantic_function(f"""Give a detailed lecture on {topic} related to {self.expertise_area}.""", max_tokens=150)(topic)
 
+    async def answer_question(self, question):
+        return kernel.create_semantic_function(f"""Provide a detailed answer to the following question in the context of {self.expertise_area}: {{$INPUT}}""")(question)
 
+class StudentAgent:
+    def __init__(self, knowledge_level, personality_type):
+        self.knowledge_level = knowledge_level
+        self.personality_type = personality_type
 
+    async def generate_questions(self, lecture_content):
+        return kernel.create_semantic_function(f"""Given your knowledge level of {self.knowledge_level} and personality type {self.personality_type}, what questions do you have about this lecture: {{$INPUT}}""")(lecture_content)
 
+    async def discuss_with_peer(self, peer, lecture_content):
+        # This function simulates discussion between two students
+        pass
 
-async def main():
-    input_doc = ["Groups", "Rings", "Fields"]
-    for section in input_doc:
-        context = kernel.CreateNewContext(input_doc)
-        lecture = LectureAgent.give_lecture(context)
-        print(lecture)
+# Main simulation function
+async def simulate_classroom():
+    # Create Professor and Student Agents
+    professor = ProfessorAgent("Mathematics")
+    students = [StudentAgent("partial", "introverted"), StudentAgent("partial", "extroverted")]
 
-        question = StudentAgent.generate_questions(context)
-        print(question)
+    # Simulate classroom interaction
+    lecture_topic = "Groups, Rings, Fields"
+    lecture_content = await professor.give_lecture(lecture_topic)
 
-        answer = LectureAgent.ask_question(question)
-        print(answer)
+    for student in students:
+        question = await student.generate_questions(lecture_content)
+        answer = await professor.answer_question(question)
+        # Log the interaction for analysis
 
-        loaded = StudentAgent.load_question_answer((question, answer))
-        print(loaded)
-
-
-
-
+    # Simulate break time interaction
+    for i in range(len(students)):
+        for j in range(i + 1, len(students)):
+            await students[i].discuss_with_peer(students[j], lecture_content)
+            # Log the interaction for analysis
 
 # Run the main function
 if __name__ == "__main__":
     import asyncio
-
-    asyncio.run(main())
+    asyncio.run(simulate_classroom())
