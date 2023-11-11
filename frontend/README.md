b56eaf68db6483124115e91a5263165dc4cb993d
Sawyer Pollard <19192015+sawyerpollard@users.noreply.github.com>
2023-11-11 16:36:22 -0500

    Add frontend to repo

---
+++ frontend/README.md
@@ -0,0 +1,9 @@
+Make sure you have Node installed. For example, install Node with Homebrew.
+
+Change directory to the "frontend" folder.
+
+Run "npm install" to install the required packages.
+
+Run "npm run dev" to start the website.
+
+Go to http://localhost:3000 to view the website.
---
+++ frontend/src/app/FileUploader.tsx
@@ -0,0 +1,64 @@
+'use client';
+import { useState, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+
+export default function FileUploader() {
+    const [file, setFile] = useState<File | undefined>();
+    const [studentCount, setStudentCount] = useState<number>(6);
+
+    function handleSliderChange(e: React.ChangeEvent<HTMLInputElement>) {
+        e.preventDefault();
+
+        setStudentCount(parseInt(e.target.value));
+    }
+
+    async function handleSubmit(e: React.SyntheticEvent) {
+        e.preventDefault();
+
+        if (typeof file === 'undefined') return;
+
+        const formData = new FormData();
+
+        alert(file.name);
+
+        /*
+        formData.append('file', file);
+        formData.append('upload_preset', 'test-react-uploads-unsigned');
+        formData.append('api_key', import.meta.env.VITE_CLOUDINARY_API_KEY);
+
+        const results = await fetch('https://api.cloudinary.com/v1_1/colbycloud-examples/image/upload', {
+            method: 'POST',
+            body: formData
+        }).then(r => r.json());
+        */
+    }
+
+    const onDrop = useCallback((acceptedFiles: Array<File>) => {
+        setFile(acceptedFiles[0]);
+    }, []);
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+    return (
+        <form onSubmit={handleSubmit} className="flex flex-col items-center gap-12">
+            <div className="flex flex-col items-center gap-4">
+                <p className="text-indigo-700 text-3xl"><span className="font-bold">{studentCount}</span> {studentCount == 1 ? 'Student' : 'Students'}</p>
+                <input onChange={handleSliderChange} value={studentCount} className="w-48 accent-indigo-700 hover:cursor-pointer" type="range" min="1" max="12" step="1" />
+            </div>
+
+            <div className="flex items-center justify-center p-8 w-64 h-64 bg-indigo-100 hover:bg-indigo-200 border-2 text-indigo-700 border-indigo-700 rounded-2xl hover:cursor-pointer scale-90 hover:scale-100 transition overflow-hidden" {...getRootProps()}>
+                <input {...getInputProps()} />
+                <div className="text-center text-xl">
+                    {file && <p className="break-all"><span className="font-bold">{file.name}</span> uploaded!</p>}
+                    {
+                        !file && (isDragActive ?
+                            <p className="font-bold">Drop files here ...</p> :
+                            <p><span className="font-bold">Click here</span> or drag files to upload ...</p>)
+                    }
+                </div>
+            </div>
+
+            <button className={`font-bold transition text-2xl border-2 border-indigo-700 rounded-xl px-6 py-4 scale-90 ${file ? 'bg-indigo-700 text-white hover:cursor-pointer hover:scale-100' : 'text-indigo-700 hover:cursor-default'}`} type="submit">Submit</button>
+        </form>
+    )
+}
