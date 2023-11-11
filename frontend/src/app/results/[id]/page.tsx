34a7783e1aacbd37f84c4a1af6f5b0ab9e6efb8f
Sawyer Pollard <19192015+sawyerpollard@users.noreply.github.com>
2023-11-11 17:13:16 -0500

    Fix typo in frontend

--- frontend/src/app/results/[id]/page.tsx
+++ frontend/src/app/results/[id]/page.tsx
@@ -5,7 +5,7 @@ export default function Results({ params }: { params: { id: string } }) {
       <p className="font-mono font-bold text-xl">Task ID: {params.id}</p>
 
       <p>
-        So, here's what's going on. You upload a file and select a student count. You press the Submit button. This takes the raw file data and student count and wraps it up into an object. This object is POSTed to the API. This returns a result, which will contain a session ID. The user is then redirected to the results page and the session ID is passed a paramater to the page. This means we can display the session ID on the results page and connect back to the API to get updates.
+        So, here's what's going on. You upload a file and select a student count. You press the Submit button. This takes the raw file data and student count and wraps it up into an object. This object is POSTed to the API. This returns a result, which will contain a session ID. The user is then redirected to the results page and the session ID is passed as a paramater to the page. This means we can display the session ID on the results page and connect back to the API to get updates. These updates will be displayed on this page. The SVG images may also be added to this page.
       </p>
     </div>
   )
