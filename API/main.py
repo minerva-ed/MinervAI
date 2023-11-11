4b0907f70bb815867c694ae44d70696ceda472fc
Dhyey Mavani <dmavani25@amherst.edu>
2023-11-11 12:38:19 -0500

    update prof agent for lecture notes loading functionality example

--- API/main.py
+++ API/main.py
@@ -16,12 +16,18 @@ kernel.add_chat_service("chat-gpt", OpenAIChatCompletion("gpt-3.5-turbo", api_ke
 class ProfessorAgent:
     def __init__(self, expertise_area):
         self.expertise_area = expertise_area
+        self.lecture_notes = None
+
+    def upload_lecture_notes(self, notes):
+        self.lecture_notes = notes
 
     async def give_lecture(self, topic):
-        return kernel.create_semantic_function(f"""Give a detailed lecture on {topic} related to {self.expertise_area}.""", max_tokens=150)(topic)
-
-    async def answer_question(self, question):
-        return kernel.create_semantic_function(f"""Provide a detailed answer to the following question in the context of {self.expertise_area}: {{$INPUT}}""")(question)
+        if self.lecture_notes:
+            # Incorporating lecture notes into the lecture generation
+            return kernel.create_semantic_function(f"""Give a detailed lecture on {topic} related to {self.expertise_area}, using the following notes: {self.lecture_notes}.""", max_tokens=200)(topic)
+        else:
+            # Default lecture generation without notes
+            return kernel.create_semantic_function(f"""Give a detailed lecture on {topic} related to {self.expertise_area}.""", max_tokens=150)(topic)
 
 class StudentAgent:
     def __init__(self, knowledge_level, personality_type):
@@ -37,9 +43,13 @@ class StudentAgent:
 
 # Main simulation function
 async def simulate_classroom():
+
     # Create Professor and Student Agents
     professor = ProfessorAgent("Mathematics")
     students = [StudentAgent("partial", "introverted"), StudentAgent("partial", "extroverted")]
+
+    # Example: Upload lecture notes
+    professor.upload_lecture_notes("Here are some key points and concepts about Groups, Rings, and Fields...")
 
     # Simulate classroom interaction
     lecture_topic = "Groups, Rings, Fields"
